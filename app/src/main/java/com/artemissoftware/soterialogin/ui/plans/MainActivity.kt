2a623e66f702d6b000dedf870c3aba4590aadc39
gustavo <gustavo.isaac@gmail.com>
2021-11-13 18:30:54 +0000

    Layout fragment_plans

---
+++ app/src/main/java/com/artemissoftware/soterialogin/ui/plans/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.artemissoftware.soterialogin.ui.plans
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.artemissoftware.soterialogin.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/com/artemissoftware/soterialogin/ui/plans/PlansFragment.kt
@@ -0,0 +1,15 @@
+package com.artemissoftware.soterialogin.ui.plans
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.artemissoftware.soterialogin.R
+
+
+class PlansFragment : Fragment(R.layout.fragment_plans) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
