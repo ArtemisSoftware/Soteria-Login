bfb1c6c10f24615961d26cc86cb642ead4ba91a9
gustavo <gustavo.isaac@gmail.com>
2021-11-13 18:21:54 +0000

    Layout fragment_discounts

---
+++ app/src/main/java/com/artemissoftware/soterialogin/ui/tutorial/DiscountsFragment.kt
@@ -0,0 +1,16 @@
+package com.artemissoftware.soterialogin.ui.tutorial
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.artemissoftware.soterialogin.R
+
+
+class DiscountsFragment : Fragment(R.layout.fragment_discounts) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
