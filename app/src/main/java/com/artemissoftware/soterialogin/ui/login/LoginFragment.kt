5e2dfdd78eb8f27afee99a4b2fe29a2ec6ba82c0
gustavo <gustavo.isaac@gmail.com>
2021-11-13 16:31:50 +0000

    Layout fragment_login - part 1

---
+++ app/src/main/java/com/artemissoftware/soterialogin/ui/login/LoginFragment.kt
@@ -0,0 +1,17 @@
+package com.artemissoftware.soterialogin.ui.login
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.artemissoftware.soterialogin.R
+
+
+class LoginFragment : Fragment(R.layout.fragment_login) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+}
