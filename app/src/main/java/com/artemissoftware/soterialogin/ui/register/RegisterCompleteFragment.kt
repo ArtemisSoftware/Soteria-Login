2479d004087c20b899cbc411d4ee2f7e5883f22a
gustavo <gustavo.isaac@gmail.com>
2021-11-13 18:13:02 +0000

    Layout fragment_register_complete

---
+++ app/src/main/java/com/artemissoftware/soterialogin/ui/register/RegisterCompleteFragment.kt
@@ -0,0 +1,16 @@
+package com.artemissoftware.soterialogin.ui.register
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.artemissoftware.soterialogin.R
+
+
+class RegisterCompleteFragment : Fragment(R.layout.fragment_register_complete) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
