99259f02b67f6ec020d24274bf57efa387775f77
gustavo <gustavo.isaac@gmail.com>
2021-11-13 18:26:58 +0000

    Layout fragment_onboarding_help

--- app/src/main/java/com/artemissoftware/soterialogin/ui/tutorial/OnboardingDiscountsFragment.kt
+++ app/src/main/java/com/artemissoftware/soterialogin/ui/tutorial/OnboardingDiscountsFragment.kt
@@ -2,13 +2,11 @@ package com.artemissoftware.soterialogin.ui.tutorial
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import com.artemissoftware.soterialogin.R
 
 
-class DiscountsFragment : Fragment(R.layout.fragment_discounts) {
+class OnboardingDiscountsFragment : Fragment(R.layout.fragment_onboarding_discounts) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
---
+++ app/src/main/java/com/artemissoftware/soterialogin/ui/tutorial/OnboardingHelpFragment.kt
@@ -0,0 +1,15 @@
+package com.artemissoftware.soterialogin.ui.tutorial
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.artemissoftware.soterialogin.R
+
+
+class OnboardingHelpFragment : Fragment(R.layout.fragment_onboarding_help) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
